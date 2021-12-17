1e54c9f254ed212c6aaa7b5727620f4f70d69a51
azizyahyaoui <azizyahyaoui716@gmail.com>
2021-12-17 02:18:43 +0100

    Update_1

--- Assets/Scripts/CamiraFollower.cs
+++ Assets/Scripts/CamiraFollower.cs
@@ -9,14 +9,43 @@ public class CamiraFollower : MonoBehaviour
     [SerializeField]
      Vector3 posOffset;
     private Vector3 playerVelocity;
+
+    [SerializeField]
+     float leftLimit;
+    [SerializeField]
+    float rightLimit;
+    [SerializeField]
+    float bottomLimit;
+    [SerializeField]
+    float topLimit;
+
     // Update is called once per frame
 
-   
+
     void Update()
     {
         transform.position = Vector3.SmoothDamp(transform.position,
             Player.transform.position + posOffset,
             ref playerVelocity, timeOffset);
-      
+
+        transform.position = new Vector3(
+            Mathf.Clamp(transform.position.x, leftLimit, rightLimit),
+            Mathf.Clamp(transform.position.y, bottomLimit, topLimit ),
+            transform.position.z
+            );
+    }
+
+    // draw a box boundary limit to camera 
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        //top boundary line
+        Gizmos.DrawLine(new Vector2(leftLimit, topLimit), new Vector2(rightLimit, topLimit));
+        //right boundary line
+        Gizmos.DrawLine(new Vector2(rightLimit, topLimit), new Vector2(rightLimit, bottomLimit));
+        //bottom boundary line
+        Gizmos.DrawLine(new Vector2(rightLimit, bottomLimit), new Vector2(leftLimit, bottomLimit));
+        //left boundary liner
+        Gizmos.DrawLine(new Vector2(leftLimit, bottomLimit), new Vector2(leftLimit, topLimit));
     }
 }
